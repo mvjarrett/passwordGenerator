501f92d0b03afe048cef4274ed259edfbc544f1e
mvjarrett <mvjarrett92@gmail.com>
2021-12-14 16:18:44 -0500

    Add function for password generate, Copy, Toast

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ function newPassword() {
  const characters = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!#$%&/()=?";
  const length = 16;
  let password = '';
+ for (let i = 0; i < length; i++) {
+  let randomNumber = Math.floor(Math.random() * characters.length);
+  password += characters.substring(randomNumber, randomNumber + 1)
+ }
+ document.getElementById('password').value = password
+ 
 }
 
-
-//write es6 for loop to iterate over the string "characters" using Math.floor(Math.random() * characters.length)
-//i konw how to do this with a "for var = i, i < length, i++;", but i want to do this using es6.
-
-// use 
-document.getElementById('password').value = password
-//to push the created password to the DOM.
+function copy() {
+  navigator.clipboard.writeText(password.value)
+  .then(() => {
+    Toastify({
+      text: "Copied Successfully!",
+      duration: 3000,
+      style: {
+        background: "linear-gradient(to right, #ae5656, #ad4444)",
+      }
+      }).showToast();
+  })
+  .catch(err => {
+    console.log('copy not successful')
+  })
+}
