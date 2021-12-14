d85c5379ed0829f59f099a12dde7c6617e61ae82
Matt <Mvjarrett92@gmail.com>
2021-12-14 00:05:11 -0500

    create HTML, CSS, JS files. Comments added.

---
+++ script.js
@@ -0,0 +1,13 @@
+function newPassword() {
+ const characters = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!#$%&/()=?";
+ const length = 16;
+ let password = '';
+}
+
+
+//write es6 for loop to iterate over the string "characters" using Math.floor(Math.random() * characters.length)
+//i konw how to do this with a "for var = i, i < length, i++;", but i want to do this using es6.
+
+// use 
+document.getElementById('password').value = password
+//to push the created password to the DOM.
